d8d241b3aecbb1a9e830d10005caac637269f8f9
Veronika Makeeva <veronikakrupina@yandex.ru>
2022-03-20 21:57:29 +0300

    unit4: added kingfisher for photos cache

--- myVKApp/myVKApp/Controllers/PhotosControllers/PhotosCollectionViewController/PhotosCollectionViewController.swift
+++ myVKApp/myVKApp/Controllers/PhotosControllers/PhotosCollectionViewController/PhotosCollectionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 //private let reuseIdentifier = "Cell"
 
@@ -27,30 +28,9 @@ class PhotosCollectionViewController: UICollectionViewController {
 				}
 			}
 		}
-		
-		
-//		self.collectionView.reloadData()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return self.photos.count
-//    }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return self.photos.count
     }
 
@@ -61,44 +41,14 @@ class PhotosCollectionViewController: UICollectionViewController {
 		}
 		
 		let url = URL(string: photos[indexPath.row].photoSizes[1].url)
-		if let data = try? Data(contentsOf: url!) {
-			photoCell.photoImageView.image = UIImage(data: data)
-		}
-    
-        // Configure the cell
+		photoCell.photoImageView.kf.setImage(with: url)
+		
+//		another way: download photo via swift:
+//		if let data = try? Data(contentsOf: url!) {
+//			photoCell.photoImageView.image = UIImage(data: data)
+//		}
     
         return photoCell
     }
-	
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
